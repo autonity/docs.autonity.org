4b5d5be87fed01e8ab37f28a7f52c8bfe2228215
Máté Szendrő <mate.szendro@clearmatics.com>
2024-09-18 14:32:49 +0200

    Add documentation generator dependencies

--- devenv.nix
+++ devenv.nix
@@ -2,6 +2,15 @@
 
 {
   packages = [
+    pkgs.git
     pkgs.quarto
+    pkgs.solc
   ];
+
+  languages.python = {
+    enable = true;
+    version = "3.11";
+    venv.enable = true;
+    venv.requirements = ./requirements.txt;
+  };
 }
