4c7739f10bab003f781b83b78c26f2b6479e6c3e
Máté Szendrő <mate.szendro@clearmatics.com>
2025-02-11 10:35:34 +0000

    Add 'site-render' and 'site-preview' commands to run apidocgen & quarto

--- devenv.nix
+++ devenv.nix
@@ -14,6 +14,7 @@
     venv.requirements = ./requirements.txt;
   };
 
+  # API doc generator commands
   scripts.apidocgen.exec = ''
     python3 -m _apidocgen $@
   '';
@@ -22,5 +23,20 @@
   '';
   scripts.lint-apidocgen.exec = ''
     ruff check _apidocgen && black --check _apidocgen && mypy _apidocgen
+  '';
+
+  # Quarto commands
+  scripts.site-render.exec = ''
+    if [ -d autonity ]; then
+      apidocgen
+    fi &&
+    quarto render
+  '';
+  scripts.site-preview.exec = ''
+    if [ -d autonity ]; then
+      apidocmon
+    else
+      quarto preview
+    fi
   '';
 }
